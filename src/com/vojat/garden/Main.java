ec787dc9df2993058716e2137d6941bc7ae7275a
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-01-10 17:44:15 +0100

    just playing with the sizes and variables

--- src/com/vojat/garden/Main.java
+++ src/com/vojat/garden/Main.java
@@ -2,8 +2,8 @@ package com.vojat.garden;
 
 public class Main{
     public static void main(String[] args) {
-        int windowWidth = 1920;
-        int windowHeight = 1080;
+        int windowWidth = 2560;
+        int windowHeight = 1440;
 
         new Game(windowWidth, windowHeight);
     }
