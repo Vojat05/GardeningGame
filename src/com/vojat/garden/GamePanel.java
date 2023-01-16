9763fe47de85a8a08ac83c12dd2ca6721042f911
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-01-16 20:06:36 +0100

    New game loop and keyboard input

--- src/com/vojat/garden/GamePanel.java
+++ src/com/vojat/garden/GamePanel.java
@@ -2,76 +2,42 @@ package com.vojat.garden;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
 
 import javax.swing.JPanel;
 
 import com.vojat.inputs.KeyboardInput;
 
-public class GamePanel extends JPanel {
-    private float positionX = 0f, positionY = 0f;
-    private float directionX = 0.15f, directionY = 0.15f;
-    private Color color = new Color(0, 0, 0);
-    private Random rand;
-    private long lastCheck = 0;
-    private int fps = 0;
-    private int windowWidth;
-    private int windowHeight;
+public class GamePanel extends JPanel{
+    private int windowWidth, windowHeight;
+    private float positionX = 10f, positionY = 10f;
 
     public GamePanel(int windowWidth, int windowHeight) { // width == window width ; height == window height
         this.windowWidth = windowWidth;
         this.windowHeight = windowHeight;
 
-        rand = new Random();
+        addKeyListener(new KeyboardInput(this));
+    }
 
-        addKeyListener(new KeyboardInput());
+    // Method to move stuff
+    // Currently moves the rectangle
+    public void moveY(float speed) {
+        if (positionY <= windowHeight - 200) {
+            positionY += speed;
+        }
+    }
 
+    public void moveX(float speed) {
+        if (positionX <= windowWidth - 225) {
+            positionX += speed;
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        updatePosition();
-        g.setColor(color);
+        moveX(5f);
+        moveY(5f);
+        g.setColor(new Color(0, 0, 0));
         g.fillRect((int) positionX, (int) positionY, 200, 150);
-
-
-        // The FPS counter
-        fps++;
-        if (System.currentTimeMillis() - lastCheck >= 1000) {
-            lastCheck = System.currentTimeMillis();
-            System.out.println("FPS: " + fps);
-            fps = 0;
-        }
-
-        super.repaint();
-    }
-
-    /*
-     * This makes the rectangle go Brrr
-     * It also reverses it's direction on a certian axis if it gets to the end of the screen
-     * Change color on colision with a border
-     */
-    private void updatePosition() {
-        if (positionX > windowWidth - 200 || positionX < 0) {
-            directionX *= -1;
-            color = getRandColor();
-        }
-        positionX += directionX;
-
-        if (positionY > windowHeight - 150 || positionY < 0) {
-            directionY *= -1;
-            color = getRandColor();
-        }
-        positionY += directionY;
-    }
-
-    // Generates random Color
-    private Color getRandColor() {
-        short r = (short) rand.nextInt(255);
-        short g = (short) rand.nextInt(255);
-        short b = (short) rand.nextInt(255);
-
-        return new Color(r, g, b);
     }
 }
--- src/com/vojat/inputs/KeyboardInput.java
+++ src/com/vojat/inputs/KeyboardInput.java
@@ -3,22 +3,45 @@ package com.vojat.inputs;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
+import com.vojat.garden.GamePanel;
+
+
 /*
  * Class implementing KeyListener interface
  * Interface must have its every method overriden
  * When extending an abstract class, you don't have to override every method
  */
 public class KeyboardInput implements KeyListener{
+
+    private GamePanel gamePanel;
+
+    public KeyboardInput (GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
     
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println(String.format("Key: %s has been pressed", e.getKeyChar()));
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                gamePanel.moveY(-5);
+                break;
+            
+            case KeyEvent.VK_S:
+                gamePanel.moveY(5);
+                break;
+            
+            case KeyEvent.VK_A:
+                gamePanel.moveX(-5);
+                break;
+
+            case KeyEvent.VK_D:
+                gamePanel.moveX(5);
+                break;
+        }
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-        System.out.println(String.format("Key: %s has been released", e.getKeyChar()));
-    }
+    public void keyReleased(KeyEvent e) {;}
 
     @Override
     public void keyTyped(KeyEvent e) {;}
