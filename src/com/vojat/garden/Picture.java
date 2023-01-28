a8269f71c71b5a4ba89bfad8e4fc5f0e7d16bd0b
Vojtěch Bartoň <78501335+Vojat05@users.noreply.github.com>
2023-01-28 19:52:00 +0100

    Images (#3)
    * The first commit to this branch
    
    * Addition of the dad character texture and it's display + movement
    
    * Move res folder out of src
    
    Implementing the Dad character texture and giving it the ability to move
    
    * Removing redundant method calls
    
    Implementing the Dad character texture and giving it the ability to move
    
    * Switching from WASD to Arrow Keys
    
    * Reading player texture protection
    
    * Fixed the KeyListener issue
    
    Component wasn't always focused
    
    * Making the method for getting texture reusable on other objects
    
    * Turning of the main character
    
    * Player movement textures done
    
    * Readme reformat
    
    * Player class added
    
    * Update changes
    
    * Fixed the turning redraw
    
    * Created the Picture class for further usage
    
    Each object will have stored the path to the picture it's meant to represent (might later be changed to a flower class for better readability)

---
+++ src/com/vojat/garden/Picture.java
@@ -0,0 +1,9 @@
+package com.vojat.garden;
+
+public class Picture {
+    private String PICTURE_PATH;
+    
+    public Picture(String path) {
+        this.PICTURE_PATH = path;
+    }
+}
--- src/com/vojat/garden/Window.java
+++ src/com/vojat/garden/Window.java
@@ -4,15 +4,14 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
 
-    public Window(GamePanel gamePanel, int width, int height) {
+    public Window(GamePanel gamePanel) {
 
-        setSize(width, height); // 2K == 2560x1440 ; Full HD == 1920x1080 ;; Resolution is stored in Main class
         setTitle("Dad The Gardener");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // Center the window on screen if it isn't maximized
-        //setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setResizable(false);
         
         add(gamePanel);
+        pack();     // Create a window that fits the JPanel passed in this constructor
         setVisible(true);
     }
 }
