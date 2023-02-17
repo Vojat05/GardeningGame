3b361ecd8443c1dbb25c1b235db7afbf8691f325
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-02-17 16:52:40 +0100

    Some more menu buttons

--- src/com/vojat/garden/Main.java
+++ src/com/vojat/garden/Main.java
@@ -11,9 +11,9 @@ import javax.swing.JPanel;
 
 public class Main{
     public static void main(String[] args) {
-        JFrame menuWindow = new JFrame();
+        JFrame menuWindow = new JFrame();                                                                   // Create the menu window
 
-        JButton start = new JButton("Start");
+        JButton start = new JButton("Start Game");                                                         // Create the start game button
         {
             start.addActionListener(new ActionListener() {
 
@@ -23,16 +23,46 @@ public class Main{
                     new Game(1920, 1080);
                 }
             });
+            start.setPreferredSize(new Dimension(150, 40));
+            start.setFocusPainted(false);
         }
 
-        JPanel buttonPanel = new JPanel();
+        JButton load = new JButton("Load Game");
+        {
+            load.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Game load button presed");
+                }
+            });
+            load.setPreferredSize(new Dimension(150, 40));
+            load.setFocusPainted(false);
+        }
+
+        JButton exit = new JButton("Quit");
+        {
+            exit.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    menuWindow.dispose();
+                }
+            });
+            exit.setPreferredSize(new Dimension(150, 40));
+            exit.setFocusPainted(false);
+        }
+
+
+        JPanel buttonPanel = new JPanel();                                                                  // Create the button panel
         {
             buttonPanel.setPreferredSize(new Dimension(200, 600));
             buttonPanel.setBackground(Color.CYAN);
+
             buttonPanel.add(start);
+            buttonPanel.add(load);
+            buttonPanel.add(exit);
         }
 
-        MenuPanel menu = new MenuPanel(1920, 1080, buttonPanel);
+        MenuPanel menu = new MenuPanel(1920, 1080, buttonPanel);                   // Create the entire menu as an object of MenuPanel
         {
             menuWindow.setPreferredSize(new Dimension(1920, 1080));
             menuWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,6 +73,5 @@ public class Main{
             menuWindow.pack();
             menuWindow.setVisible(true);
         }
-        // new Game(1920, 1080);
     }
 }
