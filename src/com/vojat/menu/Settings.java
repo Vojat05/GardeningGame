d5ac258d5caa61aefa325e28e966c4b969012cb6
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-03-03 00:39:29 +0100

    Added the controls JSON

--- src/com/vojat/menu/Settings.java
+++ src/com/vojat/menu/Settings.java
@@ -2,6 +2,7 @@ package com.vojat.menu;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,6 +11,26 @@ public class Settings extends JPanel{
     public static boolean visible = false;
 
     public Settings(int sizeX, int sizeY, JPanel buttonPanel, JPanel spacer) {
+        JPanel buttons = new JPanel();
+        {
+            buttons.setPreferredSize(new Dimension(200, 500));
+            buttons.setBackground(null);
+            buttons.setVisible(true);
+        }
+        JPanel options = new JPanel();
+        {
+            options.setLayout(new GridLayout(7, 1));
+            options.setPreferredSize(new Dimension(sizeX-300, sizeY-60));
+            options.setBackground(null);
+            options.setVisible(true);
+        }
+        JPanel line = new JPanel();
+        {
+            line.setPreferredSize(new Dimension(5, 800));
+            line.setBackground(Color.BLACK);
+            line.setVisible(true);
+        }
+
         setPreferredSize(new Dimension(sizeX, sizeY));
         setBackground(Color.DARK_GRAY);
         setVisible(visible);
@@ -19,7 +40,33 @@ public class Settings extends JPanel{
             MenuPanel.buttonSetup(back, 150, 40);
             back.addActionListener((e) -> changeVisibility(buttonPanel, spacer));
         }
-        add(back);
+        JButton save = new JButton("Save");
+        {
+            MenuPanel.buttonSetup(save, 150, 40);
+            save.addActionListener((e) -> System.out.println("Save game settings into external file"));
+        }
+        JButton restore = new JButton("Restore Controls");
+        {
+            MenuPanel.buttonSetup(restore, 150, 40);
+            restore.addActionListener((e) -> System.out.println("Restore controls to default"));
+        }
+
+        {
+            buttons.add(back);
+            buttons.add(restore);
+            buttons.add(save);
+        }
+
+        {
+            add(buttons);
+            add(line);
+            add(options);
+        }
+
+        for (int i=0; i<7; i++) {
+            JButton button = new JButton("Button " + i);
+            options.add(button);
+        }
     }
 
     public void changeVisibility(JPanel buttonPanel, JPanel spacer) {
