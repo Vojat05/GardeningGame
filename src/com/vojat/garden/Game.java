ea75055cff940dd8e55b6a2c7f16f4ba2c86f131
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-03-19 02:04:02 +0100

    Added the defaul game music and moved flower list to Game class

--- src/com/vojat/garden/Game.java
+++ src/com/vojat/garden/Game.java
@@ -1,19 +1,30 @@
 package com.vojat.garden;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import com.vojat.menu.Window;
 
 public class Game implements Runnable{
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_RESET = "\u001B[0m";
+    public ArrayList<Flower> flowers = new ArrayList<>();
     public static byte[][] map = new byte[8][15];                                                                   // [Y][X] coords  -> Now it's a total of 120 spots to place a flower
     public static String[] textures = {"res/Pics/Water_Can.png", "res/Pics/Red_Tulip.png", "res/Pics/Blue_Rose.png"};              // Array of texture paths
     private GamePanel gamePanel;
     private Thread gameLoop;
     private final int FPS_SET = 120;
-    private static boolean run = true;
+    public static boolean run = true;
 
     public Game(int panelWidth, int panelHeight, Window window) {
-        gamePanel = new GamePanel(panelWidth, panelHeight, window);
+        gamePanel = new GamePanel(panelWidth, panelHeight, window, this);
         InventoryPanel inventoryPanel = new InventoryPanel(panelWidth, panelHeight, gamePanel, gamePanel.dad);                          // Creates a new InventoryPanel object to pass into the main panel
         MainPanel mainPanel = new MainPanel(gamePanel, inventoryPanel);
         window.setElements(mainPanel);
@@ -33,6 +44,19 @@ public class Game implements Runnable{
         run = false;
     }
 
+    public static void playMusic(String path) {
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(path));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            System.gc();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.err.println("Audio error has occured!");
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void run() {                                                                                                                 // Game Loop
 
@@ -42,6 +66,17 @@ public class Game implements Runnable{
         short fps = 0;
         long lastCheck = System.currentTimeMillis();
         double deltaF = 0;
+        Clip clip = null;
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File("res/Audio/GameMusic.wav"));
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.setFramePosition(0);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.err.println("Audio error has occured!");
+            e.printStackTrace();
+        }
 
         while (run) {                                                                                                                  // While this loop runs, the game updates
             now = System.nanoTime();
@@ -59,11 +94,16 @@ public class Game implements Runnable{
                 lastCheck = System.currentTimeMillis();
                 System.out.println(ANSI_GREEN + "FPS: " + fps + ANSI_RESET);
                 fps = 0;
+                if (!clip.isRunning()) {
+                    clip.setFramePosition(0);
+                    clip.start();
+                }
             }
         }
+        clip.stop();
     }
 
-    public static void saveGame() {;}
+    public static void saveGame(File saveFile) {;}
 
-    public static void loadGame(byte[][] map) {;}
+    public static void loadGame(File saveFile) {;}
 }
