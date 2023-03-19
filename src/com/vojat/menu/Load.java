b0cdf205395f74e161ee88a02ac3517a65087e55
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-03-19 14:26:40 +0100

    Load menu button setup functional

--- src/com/vojat/menu/Load.java
+++ src/com/vojat/menu/Load.java
@@ -3,6 +3,7 @@ package com.vojat.menu;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,34 +11,40 @@ import javax.swing.JPanel;
 public class Load extends JPanel{
     public static boolean visible = false;
     private static JPanel saves = new JPanel();
+    private static ArrayList<JPanel> blocks = new ArrayList<>();
 
     public Load(int sizeX, int sizeY, JPanel buttonPanel, JPanel spacer) {
         setBackground(null);
         setVisible(visible);
         setFocusable(true);
 
-        {
-            saves.setLayout(new GridLayout(6, 1));
-            saves.setPreferredSize(new Dimension(sizeX-300, sizeY-40));
-            saves.setBackground(Color.BLACK);
-            saves.setVisible(true);
-        }
-
         JButton back = new JButton("Back");
         {
             MenuPanel.buttonSetup(back, 150, 40);
             back.addActionListener((e) -> {
                 changeVisibility(buttonPanel, spacer);
+                for (int i=0; i<blocks.size(); i++) {
+                    saves.remove(blocks.get(i));
+                }
+                blocks.clear();
             });
             back.setBackground(new Color(25, 25, 25));
             back.setForeground(Color.WHITE);
         }
 
-        JPanel space = new JPanel();
+        JPanel space = new JPanel();        // Spacer
         {
-            space.setPreferredSize(new Dimension(200, sizeY-100));
+            space.setPreferredSize(new Dimension(200, 150));
             space.setBackground(null);
         }
+
+        JPanel line = new JPanel();             // The black line in the midle
+        {
+            line.setPreferredSize(new Dimension(5, 800));
+            line.setBackground(Color.BLACK);
+            line.setVisible(true);
+        }
+
         JPanel buttons = new JPanel();
         {
             buttons.setPreferredSize(new Dimension(200, sizeY-20));
@@ -46,8 +53,47 @@ public class Load extends JPanel{
             buttons.add(back);
         }
 
+        {
+            saves.setLayout(new GridLayout(2, 3, 50, 50));
+            saves.setPreferredSize(new Dimension(sizeX-300, sizeY-200));
+            saves.setBackground(null);
+            saves.setVisible(true);
+        }
+
         add(buttons);
+        add(line);
         add(saves);
+    }
+
+    public void createDataBlocks() {
+        for (int i=0; i<6; i++) {
+            JPanel saveBlock = new JPanel();
+            
+            saves.add(saveBlock);
+            blocks.add(saveBlock);
+
+            if (i+1 == 3 || i+1 == 6) {
+                createButton(saveBlock);
+            }
+
+            if (saveBlock.getComponentCount() == 0) {
+                saveBlock.setBackground(new Color(30, 30, 30, 40));
+            } else {
+                saveBlock.setBackground(new Color(107, 252, 3, 60));
+            }
+        }
+    }
+
+    private void buttonPress(JButton button) {
+        System.out.println("Load save number " + button.getText().charAt(button.getText().length()-1));
+    }
+
+    public void createButton(JPanel panel) {
+        JButton button = new JButton("Load save " + (blocks.indexOf(panel)+1));
+        button.addActionListener((e) -> buttonPress(button));
+        button.setFocusPainted(false);
+
+        panel.add(button);
     }
 
     public void changeVisibility(JPanel buttonPanel, JPanel spacer) {
