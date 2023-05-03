9fef788ba1416fd8afddb2634eb030111542b20f
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-05-03 13:31:26 +0200

    Entering the house complete

--- src/com/vojat/inputs/MouseInput.java
+++ src/com/vojat/inputs/MouseInput.java
@@ -31,47 +31,59 @@ public class MouseInput implements MouseListener {
         gardenerY(e);
         switch (e.getButton()) {
             case MouseEvent.BUTTON1:
-                if (dad.selectedItem > 0 && controlVariableY != 7) {
-                    if (Math.abs(controlVariableX - Game.intoMapX(dad.LOCATION_X+64)) > dad.reach || Math.abs(controlVariableY - Game.intoMapY(dad.LOCATION_Y+64)) > dad.reach) {
-                        System.err.println(ErrorList.ERR_RANGE.message);
-                    } else if (Game.map[controlVariableY][controlVariableX] >= 2) {                            // Checks if the desired area is occupied or not
-                        System.err.println(ErrorList.ERR_CANTPLANT.message);
-                    } else {                                                                            // If not, creates another Flower object to place here
-                        flower = new Flower(Game.textures[dad.selectedItem], dad.inventory[dad.selectedItem], controlVariableX, controlVariableY, "Alive", assignNumberToPlant);
-                        dad.plant(flower);
-                        Game.wirteIntoMap(controlVariableY, controlVariableX, 2);                      // Writes it's value into map
-                        assignNumberToPlant++;                                                          // Assigns the plant index
-                    }
-                } else if(dad.selectedItem == 0) {       // Stop the watering if water isn't selected or if the water is empty or is out of reach
-                    if (Math.abs(controlVariableX - Game.intoMapX(dad.LOCATION_X+64)) > dad.reach || Math.abs(controlVariableY - Game.intoMapY(dad.LOCATION_Y+64)) > dad.reach) {
-                        System.err.println(ErrorList.ERR_RANGE.message);
-                    } else if (Game.map[controlVariableY][controlVariableX] != 2) {
-                        System.err.println(ErrorList.ERR_NOPLANT.message);
-                    } else if (Game.textures[0].equals("res/Pics/WaterDrop0.png")) {
-                        System.err.println(ErrorList.ERR_WATER.message);
-                    } else {
-                        dad.water(flower, controlVariableX, controlVariableY);
-                        Game.textures[0] = "res/Pics/WaterDrop" + (Integer.parseInt(Game.textures[0].substring(18, 19))-1) + ".png";
-                        dad.gamePanel.inventoryPanel.repaintItem(dad);
+                if (dad.level == 1) {
+                    System.out.println("Interaction 1");
+                } else {
+                    if (dad.selectedItem > 0 && controlVariableY != 7) {
+                        if (Math.abs(controlVariableX - Game.intoMapX(dad.LOCATION_X+64)) > dad.reach || Math.abs(controlVariableY - Game.intoMapY(dad.LOCATION_Y+64)) > dad.reach) {
+                            System.err.println(ErrorList.ERR_RANGE.message);
+                        } else if (Game.map[controlVariableY][controlVariableX] >= 2) {                            // Checks if the desired area is occupied or not
+                            System.err.println(ErrorList.ERR_CANTPLANT.message);
+                        } else {                                                                            // If not, creates another Flower object to place here
+                            flower = new Flower(Game.textures[dad.selectedItem], dad.inventory[dad.selectedItem], controlVariableX, controlVariableY, "Alive", assignNumberToPlant);
+                            dad.plant(flower);
+                            Game.wirteIntoMap(controlVariableY, controlVariableX, 2);                      // Writes it's value into map
+                            assignNumberToPlant++;                                                          // Assigns the plant index
+                        }
+                    } else if(dad.selectedItem == 0) {       // Stop the watering if water isn't selected or if the water is empty or is out of reach
+                        if (Math.abs(controlVariableX - Game.intoMapX(dad.LOCATION_X+64)) > dad.reach || Math.abs(controlVariableY - Game.intoMapY(dad.LOCATION_Y+64)) > dad.reach) {
+                            System.err.println(ErrorList.ERR_RANGE.message);
+                        } else if (Game.map[controlVariableY][controlVariableX] != 2) {
+                            System.err.println(ErrorList.ERR_NOPLANT.message);
+                        } else if (Game.textures[0].equals("res/Pics/WaterDrop0.png")) {
+                            System.err.println(ErrorList.ERR_WATER.message);
+                        } else {
+                            dad.water(flower, controlVariableX, controlVariableY);
+                            Game.textures[0] = "res/Pics/WaterDrop" + (Integer.parseInt(Game.textures[0].substring(18, 19))-1) + ".png";
+                            dad.gamePanel.inventoryPanel.repaintItem(dad);
+                        }
                     }
                 }
                 System.gc();
                 break;
             
             case MouseEvent.BUTTON2:
-                Game.getMapData("print");
+                if (dad.level == 1) {
+                    System.out.println("Interaction 2");
+                } else {
+                    Game.getMapData("print");
 
-                if (Main.debug) {
-                    try {
-                        Game.saveGame("src/com/vojat/Data/Saves/Save3.json");
-                    } catch (FileNotFoundException f) {
-                        System.err.println(ErrorList.ERR_404.message);
+                    if (Main.debug) {
+                        try {
+                            Game.saveGame("src/com/vojat/Data/Saves/Save3.json");
+                        } catch (FileNotFoundException f) {
+                            System.err.println(ErrorList.ERR_404.message);
+                        }
                     }
                 }
                 break;
 
             case MouseEvent.BUTTON3:
-                if (Math.abs(1 - Game.intoMapX(dad.LOCATION_X+64)) <= dad.reach && Math.abs(5 - Game.intoMapY(dad.LOCATION_Y+64)) <= dad.reach) dad.waterRefill(); else System.err.println(ErrorList.ERR_WELL.message);
+                if (dad.level == 1) {
+                    System.out.println("Interaction 3");
+                } else {
+                    if (Math.abs(1 - Game.intoMapX(dad.LOCATION_X+64)) <= dad.reach && Math.abs(5 - Game.intoMapY(dad.LOCATION_Y+64)) <= dad.reach) dad.waterRefill(); else System.err.println(ErrorList.ERR_WELL.message);
+                }
                 break;
         }
     }
