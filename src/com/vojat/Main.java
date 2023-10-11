ded69ae5d5191ec9edeee693f98792a002db1b4a
Vojat <78501335+Vojat05@users.noreply.github.com>
2023-10-11 23:04:26 +0200

    JAR corruption test

--- src/com/vojat/Main.java
+++ src/com/vojat/Main.java
@@ -55,7 +55,7 @@ public class Main {
             Game.langFileName = jsonEditor.readData("Language");
             Game.texturePack = jsonEditor.readData("Texture-Pack");
             Game.version = jsonEditor.readData("Version");
-            Game.FPS_SET = Byte.parseByte(jsonEditor.readData("FPS"));
+            //Game.FPS_SET = Byte.parseByte(jsonEditor.readData("FPS"));
             Game.setDayLasts(Integer.parseInt(jsonEditor.readData("Day-Lasts")));
             Game.setNightLasts(Integer.parseInt(jsonEditor.readData("Night-Lasts")));
             Game.dayNightTransitionSpeed = Double.parseDouble(jsonEditor.readData("Cycle-Transition-Value"));
