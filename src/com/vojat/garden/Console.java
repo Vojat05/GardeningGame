12f80153f6cbe996b7caabbc1fe43b8e15672509
Vojat <78501335+Vojat05@users.noreply.github.com>
2024-04-16 20:46:00 +0200

    Console output text

--- src/com/vojat/garden/Console.java
+++ src/com/vojat/garden/Console.java
@@ -18,6 +18,7 @@ public class Console {
     public static String commandPrompt = "";                                                                            // The command that is being entered to the command prompt
     public static char cursor = '|';                                                                                    // Cursor shown at the end of the command prompt
     private static boolean show = false;                                                                                // Should the console JPanel be drawn?
+    private static String[] outputStack = new String[12];                                                               // The history of entered commands and their output
     private static ArrayList<String> commandList = new ArrayList<>();                                                   // The comand queue
     private static ArrayList<String> commandArgs = new ArrayList<>();                                                   // The command arguments for the command that's to be executed
     private static HashMap<String, Integer> commandDecoder = new HashMap<String, Integer>();                            // Look-up table for decoding the instructions
@@ -37,13 +38,20 @@ public class Console {
     // Gets the command array list
     public static ArrayList<String> getCommandList() { return commandList; }
 
+    // Gets the output stack
+    public static String[] getOutput() { return outputStack; }
+
     // Sets the console show parametr to a specified value
     public static boolean setShow(boolean visible) { return show = visible; }
 
     // Switches console show parametr between the 2 possible states
     public static boolean toggleShow() { return show = show ? false : true; }
 
+    // Is the command promt displayed
     public static boolean isVisible() { return show; }
+
+    // Cleares the whole output stack
+    public static void clearOutput() { for (int i=0; i<outputStack.length; i++) outputStack[i] = null; }
 
     // Pops the firt command in the array list, working like a queue
     public static String commandPop() {
@@ -104,112 +112,119 @@ public class Console {
     public static void execChain() {
         if (Console.getCommandList().size() > 0) {
 
-                ArrayList<String> cArgs = Console.getArgsAsList();
-    
-                for (int i=0; i<Console.getCommandList().size(); i++) {
+            ArrayList<String> cArgs = Console.getArgsAsList();
 
-                    String command = Console.commandPop();
-                    switch (Console.getCommand(command).toUpperCase()) {
-    
-                        // Teleport
-                        case "TPA":
-                            if (Integer.parseInt(cArgs.get(0)) > Game.map.getColumns()) {
+            for (int i=0; i<Console.getCommandList().size(); i++) {
+                String command = Console.commandPop();
+                switch (Console.getCommand(command).toUpperCase()) {
 
-                                System.err.println(Game.ANSI_RED + ErrorList.ERR_X_TOO_FAR.message + Game.ANSI_RESET);
-                                break;
-
-                            } else if (Integer.parseInt(cArgs.get(1)) > Game.map.getRows()) {
-
-                                System.err.println(Game.ANSI_RED + ErrorList.ERR_Y_TOO_FAR.message + Game.ANSI_RESET);
-                                break;
-
-                            }
-                            Game.gamePanel.dad.LOCATION_X = GamePanel.blockWidth * Integer.parseInt(cArgs.get(0));
-                            Game.gamePanel.dad.LOCATION_Y = GamePanel.blockWidth * Integer.parseInt(cArgs.get(1));
+                    // Teleport
+                    case "TPA":
+                        if (Integer.parseInt(cArgs.get(0)) > Game.map.getColumns()) {
+                            System.err.println(Game.ANSI_RED + ErrorList.ERR_X_TOO_FAR.message + Game.ANSI_RESET);
                             break;
-
-                        case "TP":
-                            Game.gamePanel.dad.LOCATION_X = Integer.parseInt(cArgs.get(0));
-                            Game.gamePanel.dad.LOCATION_Y = Integer.parseInt(cArgs.get(1));
+                        } else if (Integer.parseInt(cArgs.get(1)) > Game.map.getRows()) {
+                            System.err.println(Game.ANSI_RED + ErrorList.ERR_Y_TOO_FAR.message + Game.ANSI_RESET);
                             break;
-
+                        }
+                        Game.gamePanel.dad.LOCATION_X = GamePanel.blockWidth * Integer.parseInt(cArgs.get(0));
+                        Game.gamePanel.dad.LOCATION_Y = GamePanel.blockWidth * Integer.parseInt(cArgs.get(1));
+                        break;
+                    
+                    case "TP":
+                        Game.gamePanel.dad.LOCATION_X = Integer.parseInt(cArgs.get(0));
+                        Game.gamePanel.dad.LOCATION_Y = Integer.parseInt(cArgs.get(1));
+                        break;
+                    
                         case "KILL", "SUICIDE", "DIE":
-                            Game.gamePanel.dad.kill();
-                            break;
+                        Game.gamePanel.dad.kill();
+                        break;
+                    
+                    case "SETSKIN":
+                        if (cArgs.size() == 0) break;
+                        if (cArgs.size() > 1) break;
+                        if (cArgs.get(0).charAt(0) < 48 && cArgs.get(0).charAt(0) > 51) break;
+                        Game.gamePanel.dad.setTextureModifier(cArgs.get(0).charAt(0));
+                        break;
+                    
+                    case "REVIVE":
+                        Game.gamePanel.dad.setHealth((byte) 100);
+                        Game.gamePanel.dad.tire((byte) -100);
+                        Game.gamePanel.dad.setTexture("Player/Dad_Texture_F" + Game.gamePanel.dad.getTextureModifier() + ".png");
+                        Game.warning = false;
+                        Game.alert = false;
+                        if (Game.gamePanel.dad.outOfStamina) {
+                            
+                            Game.gamePanel.dad.outOfStamina = false;
+                            Game.gamePanel.dad.setTexture("Player/Dad_Texture_" + (Game.gamePanel.dad.VECTORX > 0 ? "R" : "L") + Game.gamePanel.dad.getTextureModifier() + ".png");
+                            Game.gamePanel.dad.setMove(true);
+                            Game.gamePanel.getKeyboardInput().resetMovement();
+                            Game.gamePanel.dad.VECTORX = 0;
+                            Game.gamePanel.dad.VECTORY = 0;
                         
-                        case "SETSKIN":
-                            if (cArgs.size() == 0) break;
-                            if (cArgs.size() > 1) break;
-                            if (cArgs.get(0).charAt(0) < 48 && cArgs.get(0).charAt(0) > 51) break;
-                            Game.gamePanel.dad.setTextureModifier(cArgs.get(0).charAt(0));
-                            break;
-
-                        case "REVIVE":
-                            Game.gamePanel.dad.setHealth((byte) 100);
-                            Game.gamePanel.dad.tire((byte) -100);
-                            Game.gamePanel.dad.setTexture("Player/Dad_Texture_F" + Game.gamePanel.dad.getTextureModifier() + ".png");
-                            Game.warning = false;
-                            Game.alert = false;
-                            if (Game.gamePanel.dad.outOfStamina) {
-                                
-                                Game.gamePanel.dad.outOfStamina = false;
-                                Game.gamePanel.dad.setTexture("Player/Dad_Texture_" + (Game.gamePanel.dad.VECTORX > 0 ? "R" : "L") + Game.gamePanel.dad.getTextureModifier() + ".png");
-                                Game.gamePanel.dad.setMove(true);
-                                Game.gamePanel.getKeyboardInput().resetMovement();
-                                Game.gamePanel.dad.VECTORX = 0;
-                                Game.gamePanel.dad.VECTORY = 0;
-                            
-                            }
-                            break;
-
-                        case "QUIT", "EXIT":
-                            System.exit(0);
-                            break;
+                        }
+                        break;
+                    
+                    case "QUIT", "EXIT":
+                        System.exit(0);
+                        break;
+                    
+                    case "SETHP":
+                        Game.gamePanel.dad.setHealth(Byte.parseByte(cArgs.get(0)));
+                        break;
+                    
+                    case "SETSTAMINA":
+                        Game.gamePanel.dad.setStamina(Byte.parseByte(cArgs.get(0)));
+                        Game.gamePanel.inventoryPanel.SColor = new Color(0xfadc05);
+                        break;
+                    
+                    case "SAVE":
+                        try {
                         
-                        case "SETHP":
-                            Game.gamePanel.dad.setHealth(Byte.parseByte(cArgs.get(0)));
-                            break;
+                            Game.saveGame("../com/vojat/Data/Saves/Save" + cArgs.get(0) + ".json", Game.gamePanel.dad, Byte.parseByte(cArgs.get(0)));
                         
-                        case "SETSTAMINA":
-                            Game.gamePanel.dad.setStamina(Byte.parseByte(cArgs.get(0)));
-                            Game.gamePanel.inventoryPanel.SColor = new Color(0xfadc05);
-                            break;
+                        } catch (IOException ioe) {
+                            System.err.println(ErrorList.ERR_IO.message);
+                        
+                        }
+                        break;
+                    
+                    case "SUMMON":
+                        if (cArgs.get(0).toUpperCase().equals("BIRD")) {
+                            if (cArgs.size() == 2) for (int j=0; j<Integer.parseInt(cArgs.get(1)); j++) Game.spawnBird();
+                            else Game.spawnBird();
+                        }
+                        break;
+                    
+                    case "ENTITY":
+                        if (cArgs.size() == 0) break;
+                        if (cArgs.get(0).toUpperCase().equals("CLEAR")) Game.birdList.clear();
+                        break;
 
-                        case "SAVE":
-                            try {
-                            
-                                Game.saveGame("../com/vojat/Data/Saves/Save" + cArgs.get(0) + ".json", Game.gamePanel.dad, Byte.parseByte(cArgs.get(0)));
-                            
-                            } catch (IOException ioe) {
-
-                                System.err.println(ErrorList.ERR_IO.message);
-                            
-                            }
-                            break;
-
-                        case "SUMMON":
-                            if (cArgs.get(0).toUpperCase().equals("BIRD")) {
-
-                                if (cArgs.size() == 2) for (int j=0; j<Integer.parseInt(cArgs.get(1)); j++) Game.spawnBird();
-                                else Game.spawnBird();
-
-                            }
-                            break;
-
-                        case "ENTITY":
-                            if (cArgs.size() == 0) break;
-                            if (cArgs.get(0).toUpperCase().equals("CLEAR")) Game.birdList.clear();
-                            break;
-
-                        case "SETSPEED":
-                            Game.gamePanel.dad.dSpeed = Float.parseFloat(cArgs.get(0));
-                            break;
-
-                        default:
-                            break;
-                    }
+                    case "CLEAR", "CLS":
+                        clearOutput();
+                        continue;
+                    
+                    case "SETSPEED":
+                        Game.gamePanel.dad.dSpeed = Float.parseFloat(cArgs.get(0));
+                        break;
+                    
+                    default: break;
                 }
+                println(command);
             }
+        }
+    }
+
+    public static void println(String line) {
+
+        // Shift all command output by 1 row
+        String[] helper = new String[outputStack.length - 1];
+        for (int i=0; i<helper.length; i++) helper[i] = outputStack[i];
+
+        // output Write through
+        outputStack[0] = line;
+        for (int i=1; i<outputStack.length; i++) outputStack[i] = helper[i - 1];
     }
 
     // Gets the argument array list
