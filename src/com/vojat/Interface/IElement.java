1b0f2a18fe809dd88363864ab06998446612e29c
Vojat <78501335+Vojat05@users.noreply.github.com>
2025-04-20 23:56:21 +0200

    Button onclick and hover lambda functions

---
+++ src/com/vojat/Interface/IElement.java
@@ -0,0 +1,6 @@
+package com.vojat.Interface;
+
+public interface IElement {
+    public void hover(Runnable hoverAction);
+    public void onClick(Runnable onclickAction);
+}
--- src/com/vojat/UI/Button.java
+++ src/com/vojat/UI/Button.java
@@ -1,5 +1,13 @@
 package com.vojat.UI;
 
-public class Button {
-    
+import com.vojat.Interface.IElement;
+
+public class Button implements IElement {
+    public void hover(Runnable hoverAction) {
+        if (hoverAction != null) hoverAction.run();
+    }
+
+    public void onClick(Runnable onclickAction) {
+        if (onclickAction != null) onclickAction.run();
+    }
 }
